d44282d0f78bc0cedd6cb07630c013e6d4f13952
miswarpapuangan <miswarpapuangan@gmail.com>
2016-04-18 11:49:13 +0700

    versi 1.0.0

---
+++ Miswa_ES.dpr
@@ -0,0 +1,13 @@
+program Miswa_ES;
+
+uses
+  Forms,
+  TSP_SE in 'TSP_SE.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
